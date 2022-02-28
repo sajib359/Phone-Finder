9256197dc01ff899ea1d3aca4d7c5c741fc3a292
Sajib Hossain <sajib.sj359@gmail.com>
2022-02-28 22:51:56 +0600

    search input data load successfull

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 console.log('Js Connected successfully');
+
+const loadData = ()=>{
+        const searchBtn = document.getElementById('search-input');
+        const searchText = searchBtn.value;
+        console.log(searchText);
+        searchBtn.value=" ";
+
+        const url =`https://openapi.programming-hero.com/api/phones?search=${searchText}`
+
+    fetch(url)
+    .then(res=> res.json())
+    .then(data =>console.log(data))
+}
