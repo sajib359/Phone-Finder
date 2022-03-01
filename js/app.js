459071b343fb2c0d446e1ae9a6b947c9ecdd2607
Sajib Hossain <sajib.sj359@gmail.com>
2022-03-01 12:53:12 +0600

    display all phone from search button

--- js/app.js
+++ js/app.js
@@ -1,39 +1,52 @@
 console.log('Js Connected successfully');
 
+
 const loadData = ()=>{
         const searchBtn = document.getElementById('search-input');
         const searchText = searchBtn.value;
         console.log(searchText);
-        searchBtn.value=" ";
+        searchBtn.value="";
 
         const url =`https://openapi.programming-hero.com/api/phones?search=${searchText}`
 
     fetch(url)
     .then(res=> res.json())
-    .then(data =>displaySearch(data.phones))
+    .then(data =>displaySearch(data.data))
 }
-
 const displaySearch=phones=>{
     console.log(phones);
-    const searchDisplay = document.getElementById('display-searchResult');
     // for(let phone of phones){
     //     console.log(phone);
     // }
     phones.forEach(phone => {
+        console.log(phone);
+        let searchDisplay = document.getElementById('display-searchResult');
+        console.log(searchDisplay);
+        
         const div = document.createElement('div');
         div.classList.add('phones')
 
         div.innerHTML=`
+        <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mt-6">
         <a href="#">
-                    <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-                </a>
-                <div class="p-5">
-                    <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                    </a>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                    <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Read more
-                        <svg class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>`
+            <img  class="rounded-t-lg mx-auto pt-6 " src="${phone.image}" alt="" />
+        </a>
+        <div class="p-5">
+            
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${phone.phone_name}</h5>
+                <div>
+                <h6 class="mb-2 text-xl  tracking-tight text-gray-900 dark:text-white">Band: ${phone.brand}</h6>
+                </div>
+        
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+            <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Read more
+                <svg class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </a>
+        </div>
+    </div>
+`;
+
+                        searchDisplay.appendChild(div);
     });
 }
